4239a42b2f75cb8bcae643e9282daa22372e17cc
Stephen <stevionewton@gmail.com>
2019-01-09 12:10:35 -0500

    Updating

--- cogs/moderation_commands.py
+++ cogs/moderation_commands.py
@@ -1,12 +1,16 @@
 from random import *;
 from discord.ext import commands;
 from discord import Member;
+import constants;
 
 class Moderation_Commands:
     def __init__(self, bot):
         self.bot = bot;
 
-
+    @commands.command(pass_context=True)
+    @commands.has_any_role(constants.ROLE_ADMIN, constants.ROLE_MOD)
+    async def ban(self, ctx, member: Member):
+        await self.bot.ban(member);
 
 
 
